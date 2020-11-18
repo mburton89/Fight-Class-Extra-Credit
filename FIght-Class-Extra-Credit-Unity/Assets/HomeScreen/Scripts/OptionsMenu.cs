6242606f105526a3554611177f0d3cfaac9c4a5d
Jdmcc890 <55810535+Jdmcc890@users.noreply.github.com>
2020-11-18 15:11:35 -0600

    changed options menu to control menu.
    changed options menu to control menu. Justin's branch has the change of control mapping.

--- FIght-Class-Extra-Credit-Unity/Assets/HomeScreen/Scripts/OptionsMenu.cs
+++ FIght-Class-Extra-Credit-Unity/Assets/HomeScreen/Scripts/OptionsMenu.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class OptionsMenu : MonoBehaviour
+public class OptionssMenu : MonoBehaviour
 {
     private Button _overlayButton;
 
