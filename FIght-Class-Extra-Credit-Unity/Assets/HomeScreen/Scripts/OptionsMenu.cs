32a278803c236b581af7b8cce68bbafe1f64d73f
Jdmcc890 <55810535+Jdmcc890@users.noreply.github.com>
2020-11-18 15:58:44 -0600

    Update OptionsMenu.cs

--- FIght-Class-Extra-Credit-Unity/Assets/HomeScreen/Scripts/OptionsMenu.cs
+++ FIght-Class-Extra-Credit-Unity/Assets/HomeScreen/Scripts/OptionsMenu.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class OptionssMenu : MonoBehaviour
+public class OptionsMenu : MonoBehaviour
 {
     private Button _overlayButton;
 
