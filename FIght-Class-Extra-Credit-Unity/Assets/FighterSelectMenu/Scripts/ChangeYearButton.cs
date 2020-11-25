6b09478f671cd6f51236abb125e7f87cf586a594
JustinBaldridge <justinbaldridge@gmail.com>
2020-11-25 14:33:56 -0600

    new controls

---
+++ FIght-Class-Extra-Credit-Unity/Assets/FighterSelectMenu/Scripts/ChangeYearButton.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeYearButton : MonoBehaviour
+{
+    public CharacterSelectMenu menu;
+
+    public bool nextYear;
+
+    public static int yearIndex = 0;
+    private int maxYearIndex = 1;
+
+    public void SwitchYear()
+    {
+        if (nextYear)
+        {
+            if (yearIndex != maxYearIndex)
+            {
+                yearIndex++;
+            }
+            else
+            {
+                yearIndex = 0;
+            }
+        }
+        else
+        {
+            if (yearIndex != 0)
+            {
+                yearIndex--;
+            }
+            else
+            {
+                yearIndex = maxYearIndex;
+            }
+        }
+
+        menu.SwitchYear(yearIndex);
+    }
+}
--- FIght-Class-Extra-Credit-Unity/Assets/FighterSelectMenu/Scripts/CharacterSelectMenu.cs
+++ FIght-Class-Extra-Credit-Unity/Assets/FighterSelectMenu/Scripts/CharacterSelectMenu.cs
@@ -5,28 +5,90 @@ using UnityEngine.UI;
 
 public class CharacterSelectMenu : MonoBehaviour
 {
-    [SerializeField] private SelectableThumbnail _selectableThumbnailPrefab;
-    [SerializeField] private GridLayoutGroup _grid;
 
-    private List<SelectableThumbnail> _allThumbnails = new List<SelectableThumbnail>();
+    public int currentYear = 1; 
+
+    [SerializeField] private SelectableThumbnail _selectableThumbnailPrefab;
+    [SerializeField] private GridLayoutGroup _grid2019;
+    [SerializeField] private GridLayoutGroup _grid2020;
+
+    private List<SelectableThumbnail> _2019Thumbnails = new List<SelectableThumbnail>();
+    private List<SelectableThumbnail> _2020Thumbnails = new List<SelectableThumbnail>();
 
     [SerializeField] private CharacterGridNavigator _player1Navigator;
     [SerializeField] private CharacterGridNavigator _player2Navigator;
 
     void Start()
     {
-        for (int i = 0; i < DataReferenceManager.Instance.characterNames.Count; i++)
+        /*for (int i = 0; i < DataReferenceManager.Instance.characterNames.Count; i++)
         {
             SelectableThumbnail newThumbnail = Instantiate(_selectableThumbnailPrefab);
             newThumbnail.transform.SetParent(_grid.transform);
             newThumbnail.Init(this, i, _grid.transform);
-            _allThumbnails.Add(newThumbnail);
+            _2019Thumbnails.Add(newThumbnail);
         }
 
-        _allThumbnails[0].Select(true);
-        _allThumbnails[0].Select(false);
+        _2019Thumbnails[0].Select(true);
+        _2019Thumbnails[0].Select(false);
 
-        _player1Navigator.Init(_allThumbnails);
-        _player2Navigator.Init(_allThumbnails);
+        _player1Navigator.Init(_2019Thumbnails);
+        _player2Navigator.Init(_2019Thumbnails); */
+
+        Initialize2019();
+        Initialize2020();
+
+        SwitchYear(currentYear);
+    }
+
+    public void SwitchYear(int index)
+    {
+        Debug.Log(index);
+        if (index == 0) // Year 2019
+        {
+            _2019Thumbnails[0].Select(true);
+            _2019Thumbnails[0].Select(false);
+
+            _player1Navigator.Init(_2019Thumbnails);
+            _player2Navigator.Init(_2019Thumbnails);
+
+            _grid2019.enabled = true;
+        }
+        else if (index == 1) // Year 2020
+        {
+            _2020Thumbnails[0].Select(true);
+            _2020Thumbnails[0].Select(false);
+
+            _player1Navigator.Init(_2020Thumbnails);
+            _player2Navigator.Init(_2020Thumbnails);
+
+            _grid2020.enabled = true;
+        }
+    }
+
+    private void Initialize2019()
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            SelectableThumbnail newThumbnail = Instantiate(_selectableThumbnailPrefab);
+            newThumbnail.transform.SetParent(_grid2019.transform);
+            newThumbnail.Init(this, i, _grid2019.transform);
+            _2019Thumbnails.Add(newThumbnail);
+        }
+
+        _grid2019.enabled = false;
+
+    }
+
+    private void Initialize2020()
+    {
+        for (int i = 9; i < DataReferenceManager.Instance.characterNames.Count; i++)
+        {
+            SelectableThumbnail newThumbnail = Instantiate(_selectableThumbnailPrefab);
+            newThumbnail.transform.SetParent(_grid2020.transform);
+            newThumbnail.Init(this, i, _grid2020.transform);
+            _2020Thumbnails.Add(newThumbnail);
+        }
+
+        _grid2020.enabled = false;
     }
 }
